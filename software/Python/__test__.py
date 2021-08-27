b395dcd1dcb6d3c9ff38bde21db8224874e67560
Mihaela Petkova <mishippp@gmail.com>
2021-08-27 21:44:05 +0300

    commit
    IT`S WORK!

--- software/Python/__test__.py
+++ software/Python/__test__.py
@@ -43,26 +43,44 @@ while camera.IsGrabbing():
             cv.destroyAllWindows()
             break
 
+        elif k == ord('s'):
+            with camera.RetrieveResult(2000) as result:
+
+                for i in range(num_img_to_save):
+
+                    grabResult = camera.RetrieveResult(2000, pylon.TimeoutHandling_ThrowException)
+
+                    image = converter.Convert(grabResult)
+                    img = image.GetArray()
+
+                    filename = "../Python/img/saved_pypylon_img_%d.png" % i
+                    cv.imwrite(filename, img)
+                    cv.destroyAllWindows()
+                    time.sleep(seconds)
+
+            exit(1)
+# Releasing the resource    
+camera.StopGrabbing()
+camera.Close()
+
+'''
         #saving
         elif k == ord('s'):
                 for i in range(num_img_to_save):
                     with camera.RetrieveResult(2000) as result:
                         # Calling AttachGrabResultBuffer creates another reference to the
                         # grab result buffer. This prevents the buffer's reuse for grabbing.
-                        filename = "../Python/img/saved_pypylon_img_%d.png" % i
-                        #cv.imwrite(filename, img)
-                        cv.destroyAllWindows()
-                        
+
                         #saving first with delay - 5 seconds
                         while i != num_img_to_save :
+                            filename = "../Python/img/saved_pypylon_img_%d.png" % i
                             current_time = time.time()
                             elapsed_time = current_time - start_time
                             if elapsed_time > seconds:
                                 cv.imwrite(filename, img)
+                                cv.destroyAllWindows()
                                 print("Finished iterating in: " + str(int(elapsed_time))  + " seconds")
+                                time.sleep(seconds)
                                 break    
-#                exit(1)     
-
-# Releasing the resource    
-camera.StopGrabbing()
-camera.Close()
+            exit(1) 
+'''
