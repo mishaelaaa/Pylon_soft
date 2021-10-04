b7a05b4e63bc01f0e426e69d4d1fbe1079f2faf0
Mihaela Petkova <mishippp@gmail.com>
2021-10-04 18:28:30 +0300

    new commit
    everything is working without radio part.

--- software/Python/__main__.py
+++ software/Python/__main__.py
@@ -4,9 +4,9 @@ from pypylon import pylon
 import time 
 
 #set up for timing loop
-start_time = time.time()
-print("enter the seconds : ")
-seconds = input()
+#start_time = time.time()
+#print("enter the seconds : ")
+#seconds = input()
 
 # how img will be saved
 num_img_to_save = 5
@@ -32,7 +32,7 @@ while camera.IsGrabbing():
         # Access the image data
         image = converter.Convert(grabResult)
         img = image.GetArray()
-        cv.namedWindow('title', cv.WINDOW_AUTOSIZE) 
+        cv.namedWindow('title', cv.WINDOW_NORMAL) 
         cv.imshow('title', img)
         
         k=cv.waitKey(1)
@@ -46,6 +46,7 @@ while camera.IsGrabbing():
             with camera.RetrieveResult(2000) as result:
 
                 for i in range(num_img_to_save):
+
                     grabResult = camera.RetrieveResult(2000, pylon.TimeoutHandling_ThrowException)
                     image = converter.Convert(grabResult)
                     img = image.GetArray()
@@ -53,11 +54,10 @@ while camera.IsGrabbing():
                     filename = "../Python/img/saved_pypylon_img_%d.png" % i
                     cv.imwrite(filename, img)
                     cv.destroyAllWindows()
-                    time.sleep(seconds)
+                    #time.sleep(seconds)
+                    time.sleep(5)
             exit(1)
 
-        else:    
-            print("None")
 # Releasing the resource    
 camera.StopGrabbing()
-camera.Close()
+camera.Close() 
